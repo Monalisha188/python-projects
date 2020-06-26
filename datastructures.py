49b980cfc84877665adc72ba48618eac20f3e5a1
MONALISHA MALLICK <67420513+Monalisha188@users.noreply.github.com>
2020-06-26 12:45:40 +0530

    to find the radius of the circle

--- datastructures.py
+++ datastructures.py
@@ -1,1 +1,18 @@
 #to find the area of the circle
+import math
+radius= float (input(" Input the radius of the circle:"))
+area = math.pi* (radius**2)
+print("The area of the circle with radius", end = " "); print(radius, end= " "); print ("is:", end= " "); print( area)
+
+
+
+#to print the extension of filename
+x = input("Input the Filename:")
+s = x. split('.')
+s = s[1]
+t= "py"
+if ( s==t):
+    print(" The extension of the file is:", end = " "); print("python")
+else:
+    print(" ")
+    
