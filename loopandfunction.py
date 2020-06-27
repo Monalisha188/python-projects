ceeb9e80705f1de4dd4222831f637f3b550ab96f
MONALISHA MALLICK <67420513+Monalisha188@users.noreply.github.com>
2020-06-27 18:39:50 +0530

    code to find the fibonacci series in python
    code to find the positive numbers in a range.

--- loopandfunction.py
+++ loopandfunction.py
@@ -9,4 +9,16 @@ for i in range(n):
   b=temp+b
 
 
-#to 
+#to print all positive numbers in a range
+l=[]
+a = int(input())
+for i in range(a):
+  i = int(input())
+  if i not in l:
+    l.append(i)
+print("list1 =", l)
+my_list = l
+for j in my_list:
+  if j>=0:
+    print(j, end=" ")
+
